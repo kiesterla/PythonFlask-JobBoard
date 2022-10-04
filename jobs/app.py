7f417e8d121c97950509b0a128fbefdc294b5781
Laura Kiester <laurakiester@mail.weber.edu>
2022-10-04 15:49:30 -0600

    test

--- jobs/app.py
+++ jobs/app.py
@@ -23,6 +23,12 @@ def execute_sql(sql, values = (), commit = False, single = False):
     cursor.close()
     return results
 
+@app.route('/job/<job_id>')
+def job(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    return render_template('job.html', job=job)
+    
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
